a8b14aa492b33e2e74311bd65c47a6a4a66b438e
Travis Chase <gaudeon@cpan.org>
2017-06-24 10:59:41 -0600

    added basic attributes to entity

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,3 +1,26 @@
 export default class Entity extends Phaser.Sprite () {
-    
+    constructor (game, x, y, key, frame) {
+        super(game, x, y, key, frame);
+
+        this._attack = 0;
+        this._defense = 0;
+        this._hitPoints = 0;
+        this.speed = 0;
+    }
+
+    get attack () { return this._attack; }
+
+    set attack (val) { this._attack = val; }
+
+    get defense () { return this._defense; }
+
+    set defense (val) { this._defense = val; }
+
+    get hitPoints () { return this._hitPoints; }
+
+    set hitPoints (val) { this._hitPoints = val; }
+
+    get speed () { return this._speed; }
+
+    set speed (val) { this._speed = val; }
 }
