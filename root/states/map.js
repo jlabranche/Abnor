19f323a321f209c223285559103422a3adb68064
Jean LaBranche <jlabranche@pro-jlabranc1-m.corp.endurance.com>
2017-06-24 13:11:32 -0600

    add assets, add map.js

--- root/states/map.js
+++ root/states/map.js
@@ -1,20 +1,29 @@
 export default class MapState extends Phaser.State {
 	constructor (game) {
 		super(game);
-		this.button;
+		this.world_button;
+		this.battle_button;
 		this.background;
 	}
 
     preload () {
-	    this.game.load.spritesheet('button', '/assets/buttons/button_sprite_sheet.png', 193, 71);
+	    this.game.load.spritesheet('left-arrow', '/assets/buttons/f/Sprites/transparentDark/transparentDark22.png', 193, 71);
+	    this.game.load.spritesheet('key', '/assets/buttons/f/Sprites/transparentDark/transparentDark27.png', 193, 71);
 	}
 
 	create () {
 	    this.game.stage.backgroundColor = '#182d3b';
-	    this.button = this.game.add.button(this.game.world.centerX - 95, 400, 'button', this.actionOnClick, this, 2, 1, 0);
+	    this.world_button = this.game.add.button(25, this.game.height - 100, 'left-arrow', this.startWorldState, this, 2, 1, 0);
+	    this.battle_button = this.game.add.button(50, 25, 'key', this.startBattleState, this, 2, 1, 0);
 	}
 
     actionOnClick () {
         console.log('clicky');
     }
+    startWorldState () {
+        this.state.start('World');
+    };
+    startBattleState () {
+        this.state.start('Battle');
+    };
 }
